79b2491cf3031ba145d81a7a28337410f0a43fcd
RupamTD <rupam.pro9@gmail.com>
2022-05-15 22:57:33 +0530

    ProxyLOc Update

--- Utils/JointPlacement.py
+++ Utils/JointPlacement.py
@@ -1,6 +1,30 @@
 import maya.cmds as cmds
-
-
-
+from . import Libraries as lib
 
 # Proxy locator placement
+def ProxyLoc():
+    #Spine Skeleton Chain
+    lib.locatorPlacement('thoriac_vertibrae_C19_C',2971,3192)
+    lib.locatorPlacement('thoriac_vertibrae_C18_C',2971,3192)
+    lib.locatorPlacement('thoriac_vertibrae_C17_C', 2971, 3192)
+    lib.locatorPlacement('thoriac_vertibrae_C16_C', 2971, 3192)
+    lib.locatorPlacement('thoriac_vertibrae_C15_C', 2971, 3192)
+    lib.locatorPlacement('thoriac_vertibrae_C14_C', 2971, 3192)
+    lib.locatorPlacement('thoriac_vertibrae_C13_C', 1025, 2660)
+    lib.locatorPlacement('thoriac_vertibrae_C12_C', 1025, 2660)
+    lib.locatorPlacement('thoriac_vertibrae_C11_C', 1025, 2660)
+    lib.locatorPlacement('thoriac_vertibrae_C10_C', 1025, 2660)
+    lib.locatorPlacement('thoriac_vertibrae_C09_C', 1025, 2660)
+    lib.locatorPlacement('thoriac_vertibrae_C08_C', 2659, 1275)
+    lib.locatorPlacement('thoriac_vertibrae_C07_C', 2659, 1275)
+    lib.locatorPlacement('thoriac_vertibrae_C06_C', 325, 1319)
+    lib.locatorPlacement('thoriac_vertibrae_C05_C', 325, 1325)
+    lib.locatorPlacement('thoriac_vertibrae_C04_C', 330, 1325)
+    lib.locatorPlacement('thoriac_vertibrae_C03_C', 326, 1324)
+    lib.locatorPlacement('thoriac_vertibrae_C02_C', 338, 1324)
+    lib.locatorPlacement('thoriac_vertibrae_C01_C', 2057, 2732)
+
+
+
+
+
--- Utils/Libraries.py
+++ Utils/Libraries.py
@@ -1,3 +1,21 @@
+"""
+setup Maya
+import sys
+import importlib as importlib
+#Path import
+if rigDir in sys.path:
+    sys.path.remove(rigDir)
+rigDir = "C:/Users/rupam/PycharmProjects/Advanced-Quaruped-Autorigger"
+sys.path.append(rigDir)
+#Modules import
+import Utils
+from Utils import Libraries
+from Utils import JointPlacement
+#Functions import
+importlib.reload(JointPlacement)
+jp = Utils.JointPlacement.ProxyLoc()
+
+"""
 import maya.cmds as cmds
 
 # Locator placement : Proxy locator in joint positions
@@ -6,4 +24,6 @@ def locatorPlacement(skeletonName,vertexOne,vertexTwo):
     vertexPosTwo = cmds.pointPosition(f"{skeletonName}.vtx[{vertexTwo}]")
     #we get two list and average the values inside zip
     LocatorPos = [(x1 + x2)/2 for (x1, x2) in zip(vertexPosOne, vertexPosTwo)]
-    cmds.spaceLocator(name = f"{skeletonName}ProxyLocator",p=LocatorPos)
+    tempLocator = cmds.spaceLocator(name = f"{skeletonName}_ProxyLocator",p=LocatorPos)
+    #center pivto the locator
+    cmds.xform(tempLocator , cp=1)
